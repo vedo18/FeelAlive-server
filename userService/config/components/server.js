021aa6381f03e90f554bab5f98129bb072d91cc4
vedo18 <saketvats18@gmail.com>
2024-10-04 14:03:22 +0530

    added error handler if any env is missing

--- userService/config/components/server.js
+++ userService/config/components/server.js
@@ -1,20 +1,28 @@
 const Joi = require('joi');
 
 const schema = Joi.object({
-    NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
-    PORT: Joi.string().required(),
-    DB_STRING: Joi.string().required(),
-});
+  NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
+  PORT: Joi.string().required(),
+  DB_STRING: Joi.string().required(),
+}).unknown(); // Allow unknown keys
 
 const { error, value: envVars } = schema.validate(process.env);
 
+if (error) {
+  console.error(
+    'Config validation error:',
+    error.details.map((d) => d.message).join(', ')
+  );
+  process.exit(1);
+}
+
 const config = {
-    env: envVars.NODE_ENV,
-    isTest: envVars.NODE_ENV === 'test',
-    isDevelopment: envVars.NODE_ENV === 'development',
-    isProduction: envVars.NODE_ENV === 'production',
-    port: envVars.PORT,
-    dbString: envVars.DB_STRING,
+  env: envVars.NODE_ENV,
+  isTest: envVars.NODE_ENV === 'test',
+  isDevelopment: envVars.NODE_ENV === 'development',
+  isProduction: envVars.NODE_ENV === 'production',
+  port: envVars.PORT,
+  dbString: envVars.DB_STRING,
 };
 
 module.exports = config;
