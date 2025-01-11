ab2be499400ae31a4d03531759d97f318f44143e
vedo18 <saketvats18@gmail.com>
2025-01-11 19:47:30 +0530

    user update api

--- userService/src/v1/controllers/user.controller.js
+++ userService/src/v1/controllers/user.controller.js
@@ -2,7 +2,20 @@ const { User } = require('../../models/index');
 const { asyncHandler } = require('../../middlewares/index');
 
 module.exports.updateUser = asyncHandler(async (req, res) => {
-  res.send({ error: false, message: 'User updated successfully' });
+  const { id } = req.params;
+  const body = req.body;
+
+  const user = await User.findByPk(id);
+  if (!user) {
+    return res.status(404).send({ error: true, message: 'User not found' });
+  }
+
+  if (body.fullName) user.fullName = body.fullName;
+  if (body.email) user.email = body.email;
+
+  await user.save();
+
+  res.send({ error: false, message: 'User updated successfully', user });
 });
 
 module.exports.getUser = asyncHandler(async (req, res) => {
